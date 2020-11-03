ae9e940ee67dc24222e2cd408500e246498cf1eb
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-11-03 22:14:20 +0000

    ubung6

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -1,9 +1,53 @@
 package exercise6;
 
 import java.util.Scanner;
+import java.lang.Character; 
+import java.util.Arrays;
 
 public class PasswordCheck {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+       Scanner input = new Scanner(System.in);
+
+        boolean isValidLength;
+        boolean isValidContent;
+
+        do{
+        System.out.println("Enter a password: ");
+        String password = input.next();
+        int passwordLength = password.length();
+
+            if (passwordLength < 8) {
+                isValidLength = false;
+            } else {
+                isValidLength = true;
+            }
+
+            char[] ch = password.toCharArray();
+            int falseCounterOne = 0;
+            int trueCounterTwo = 0;
+            for (int i = 0; i < ch.length; i++) {
+                if (!Character.isLetterOrDigit(ch[i])) {
+                    falseCounterOne++;
+                }
+            }
+            for (int i = 0; i < ch.length; i++) {
+                if (Character.isDigit(ch[i])) {
+                    trueCounterTwo++;
+                }
+            }
+
+            if (falseCounterOne > 1 || trueCounterTwo < 2) {
+                isValidContent = false;
+            } else {
+                isValidContent = true;
+            }
+
+
+            if (isValidContent && isValidLength) {
+                System.out.println("Password valid!");
+            }
+        }while(!isValidContent || !isValidLength);
+
     }
 }
+        
